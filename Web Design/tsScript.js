363f4a32cc96cebaaba5ec2369a14812f89e031e
mblucas4920 <madelineblucas@gmail.com>
2024-10-02 14:58:39 -0400

    webpages

--- Web Design/tsScript.js
+++
@@ -1,6 +0,0 @@
-//creates the function "albumChange" that calls upon "iframeSrc"
-function albumChange(iframeSrc) {
-    //Searches the document(albums.html in this case) for the element with the ID "albumFrame"
-    //It then finds this elements ".src" attribute and replaces it with some value of "iframeSrc" that is given in albums.html
-    document.getElementById("albumFrame").src = iframeSrc;
-}
