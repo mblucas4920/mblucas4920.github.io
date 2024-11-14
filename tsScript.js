12a9894e6d7f9c24c1d92e7930473be6d3c4b314
mblucas4920 <madelineblucas@gmail.com>
2024-11-14 10:30:43 -0500

    CSS Changes
    Added star ratings to each album

--- tsScript.js
+++ tsScript.js
@@ -4,3 +4,25 @@ function albumChange(iframeSrc) {
     //It then finds this elements ".src" attribute and replaces it with some value of "iframeSrc" that is given in albums.html
     document.getElementById("albumFrame").src = iframeSrc;
 }
+
+
+function validateForm() {
+    const albumChoices = document.getElementsByName('favAlbum');
+    let voteSelected = false;
+
+    // Loop through the album buttons to see if one is selected
+    for (let i = 0; i < albumChoices.length; i++) {
+        if (albumChoices[i].checked) {
+            voteSelected = true;
+            break;
+        }
+    }
+
+    // If no album is selected (!voteSelected) when submit is pressed, then an alert will pop up
+    if (!voteSelected) {
+        alert("Please select an album");
+        return false;
+    }
+
+    return true; // Submit form if validation passes
+}
